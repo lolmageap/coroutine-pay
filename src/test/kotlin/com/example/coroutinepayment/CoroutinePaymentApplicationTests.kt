67503da853eb4311e0e440910ecfbd177b5ea189
cherhy <ekxk1234@naver.com>
2024-01-22 01:03:10 +0900

    create test

--- src/test/kotlin/com/example/coroutinepayment/CoroutinePaymentApplicationTests.kt
+++ src/test/kotlin/com/example/coroutinepayment/CoroutinePaymentApplicationTests.kt
@@ -1,12 +1,32 @@
 package com.example.coroutinepayment
 
+import com.example.coroutinepayment.model.Product
+import com.example.coroutinepayment.repository.ProductRepository
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-class CoroutinePaymentApplicationTests {
+@ActiveProfiles("test")
+class CoroutinePaymentApplicationTests(
+    @Autowired private val productRepository: ProductRepository,
+): StringSpec({
 
-    @Test
-    fun contextLoads() {
+    "context loads" {
+        val prevCount = productRepository.count()
+        productRepository.save(
+            Product(
+                name = "name",
+                price = 1000
+            )
+        )
+        val nextCount = productRepository.count()
+
+        nextCount shouldBe prevCount + 1
+        productRepository.deleteAll()
     }
-}
+
+})
