972cec5e9d7b7bcb813b7717f737e48df0e8f605
cherhy <ekxk1234@naver.com>
2024-01-21 23:01:14 +0900

    create project

---
+++ src/main/kotlin/com/example/coroutinepayment/CoroutinePaymentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.coroutinepayment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutinePaymentApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoroutinePaymentApplication>(*args)
+}
