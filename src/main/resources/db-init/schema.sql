6bc4ac8575e4e805b185f6c2a08267d6e8dd6b89
cherhy <ekxk1234@naver.com>
2024-01-22 01:03:35 +0900

    create sql initialize

--- src/main/resources/db-init/schema.sql
+++ src/main/resources/db-init/schema.sql
@@ -1,23 +1,29 @@
 CREATE TABLE IF NOT EXISTS product (
     id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
     name VARCHAR(255) NOT NULL,
     price BIGINT NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS "order" (
     id SERIAL PRIMARY KEY,
-    userId BIGINT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    user_id BIGINT NOT NULL,
     description VARCHAR(255),
     amount BIGINT DEFAULT 0,
-    pgOrderId VARCHAR(255),
-    pgKey VARCHAR(255),
-    pgStatus VARCHAR(50) DEFAULT 'CREATE',
-    pgRetryCount INTEGER DEFAULT 0
+    pg_order_id VARCHAR(255),
+    pg_key VARCHAR(255),
+    pg_status VARCHAR(50) DEFAULT 'CREATE',
+    pg_retry_count INTEGER DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS "product_in_order" (
-   orderId BIGINT NOT NULL,
-   productId BIGINT NOT NULL,
+   order_id BIGINT NOT NULL,
+   product_id BIGINT NOT NULL,
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP,
    price BIGINT NOT NULL,
    quantity INTEGER NOT NULL,
    seq BIGINT PRIMARY KEY
