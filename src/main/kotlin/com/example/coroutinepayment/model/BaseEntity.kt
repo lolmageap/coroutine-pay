96ffa97a9eb156e191ec60084cb6b633e1e1e958
cherhy <ekxk1234@naver.com>
2024-01-21 23:48:35 +0900

    create base entity with kassava

---
+++ src/main/kotlin/com/example/coroutinepayment/model/BaseEntity.kt
@@ -0,0 +1,24 @@
+package com.example.coroutinepayment.model
+
+import au.com.console.kassava.kotlinToString
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.io.Serializable
+import java.time.LocalDateTime
+
+open abstract class BaseEntity(
+    @CreatedDate
+    var createdAt: LocalDateTime? = null,
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null,
+): Serializable {
+    override fun toString(): String {
+        return kotlinToString(
+            arrayOf(
+                BaseEntity::createdAt,
+                BaseEntity::updatedAt,
+            )
+        )
+    }
+}
