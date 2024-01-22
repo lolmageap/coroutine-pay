b8f8cc9704ecc7803a89ec5fd0b17efabb94e9ba
cherhy <ekxk1234@naver.com>
2024-01-22 22:46:43 +0900

    create order repository

--- src/main/kotlin/com/example/coroutinepayment/repository/OrderRepository.kt
+++ src/main/kotlin/com/example/coroutinepayment/repository/OrderRepository.kt
@@ -4,4 +4,5 @@ import com.example.coroutinepayment.model.Order
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
 interface OrderRepository: CoroutineCrudRepository<Order, Long> {
+    suspend fun findAllByUserIdOrderByCreatedAtDesc(userId: Long): List<Order>
 }
